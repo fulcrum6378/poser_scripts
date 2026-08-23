aa7a7af29e383a721faac27a739c548fc5269780
Mahdi Parastesh <fulcrum1378@gmail.com>
2026-08-23 19:22:27 +0330

    2026.08.23 - 1

--- fantasy_register_render.py
+++ fantasy_register_render.py
@@ -11,15 +11,16 @@ import quick_yaml
 DESKTOP = os.environ['USERPROFILE'] + '\\Desktop'
 RENDER_CACHE_1 = DESKTOP
 RENDER_CACHE_2 = '\\\\NERA\\Renders'
+POWERSHELL = r'C:\Program Files\PowerShell\7\pwsh.exe'
 
-scene_path = poser.Scene().DocumentPath()
-album_path = os.path.dirname(scene_path)
+scene_path: str = poser.Scene().DocumentPath()
+album_path: str = os.path.dirname(scene_path)
 render_cache = RENDER_CACHE_2
 continuum: bool = os.path.basename(album_path)[2:4] == '. '
 
 # check if this is a Fantasy scene first
-album_id: str
-album_name: str
+album_id: str = ''
+album_name: str = ''
 if not continuum:
     poser.DialogSimple.MessageBox('This document is not a Fantasy scene.')
 else:
@@ -29,7 +30,8 @@ else:
 if continuum:
     have_laptop_render = False
     for file in os.listdir(RENDER_CACHE_1):
-        if file.endswith('.exr') and os.path.getsize(os.path.join(RENDER_CACHE_1, file)) > 104_857_600:  # 100 MB
+        if file.endswith('.exr') and \
+                os.path.getsize(os.path.join(RENDER_CACHE_1, file)) > 104_857_600:  # 100 MB
             have_laptop_render = True
     if have_laptop_render:
         if poser.DialogSimple.YesNo('Is it rendered in this device?') == 1:
@@ -51,7 +53,8 @@ if continuum:
     yml = os.path.join(album_path, album_name + '.yml')
     exr_path = render_cache + '\\' + choice + '.exr'
     if os.path.isfile(yml):
-        render_id = int(list(quick_yaml.load(open(yml, 'r').read()).keys())[-1]) + 1
+        render_name = list(quick_yaml.load(open(yml, 'r').read()).keys())[-1]
+        render_id = int(render_name.split('. ')[0]) + 1
     else:
         render_id = 1
     render_id = f'{render_id:03d}'
@@ -91,11 +94,11 @@ if continuum:
         for char in characters:
             command = rf'''
 $shell = New-Object -ComObject WScript.Shell
-$shortcut = $shell.CreateShortcut("{DESKTOP}\{export_name} ({char}).pz2.lnk")
+$shortcut = $shell.CreateShortcut("{DESKTOP}\{export_name} ({char}).lnk")
 $shortcut.TargetPath = "{pose_dir}\{export_name} ({char}).pz2"
 $shortcut.Save()
             '''
-            subprocess.run(["powershell.exe", "-NoProfile", "-Command", command], text=True)
+            subprocess.run([POWERSHELL, '-NoProfile', '-Command', command], text=True)
 
     # create a reminding shortcut for cropping special thumbnails for the extracted poses
     command = rf'''
@@ -107,4 +110,4 @@ $shortcut.WorkingDirectory = "{DESKTOP}"
 $shortcut.IconLocation = "%ProgramFiles%\Adobe\Adobe Photoshop CS6 (64 Bit)\Photoshop.exe,36"
 $shortcut.Save()
     '''
-    subprocess.run(["powershell.exe", "-NoProfile", "-Command", command], text=True)
+    subprocess.run([POWERSHELL, '-NoProfile', '-Command', command], text=True)

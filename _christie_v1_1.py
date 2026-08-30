7a5fd4f02156336acca34e70e66016cb11a75940
Mahdi Parastesh <fulcrum1378@gmail.com>
2026-08-30 06:49:56 +0330

    2026.08.30 - 1

--- _christie_v1_1.py
+++ _christie_v1_1.py
@@ -3,6 +3,8 @@ figure = poser.Scene().CurrentFigure()
 if figure.Name() != 'CHRISTIE':
     figure.SetName('CHRISTIE')
 
+    figure.SetSkinType(3)  # Poser Unimesh
+
     # body = figure.Actor('BODY')
     # calling body.LoadMaterialCollection() here makes Poser crash!
 
--- _luna_v4_6.py
+++ _luna_v4_6.py
@@ -3,9 +3,13 @@ figure = poser.Scene().CurrentFigure()
 if figure.Name() != 'LUNA':
     figure.SetName('LUNA')
 
+    figure.SetSkinType(3)  # Poser Unimesh
+    figure.SetNumbSubdivRenderLevels(1)
+
     body = figure.Actor('BODY')
     body.CreateValueParameter('Muscular')
     body.CreateValueParameter('Giantess')
+    body.CreateValueParameter('Svelte')
     # calling body.LoadMaterialCollection() here makes Poser crash!
 
 else:
